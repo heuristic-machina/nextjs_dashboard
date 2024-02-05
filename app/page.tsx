2a08f47f9a2dd1355444e9f48d97c6dee28fafb8
heuristic_machina <you@example.com>
2024-02-05 02:03:56 -0700

    image classes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import styles from '@/app/ui/css/home.module.css';
+// import styles from '@/app/ui/css/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
@@ -13,8 +13,8 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center bg-gray-500 gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className={styles.shape} />
+        <div className="flex flex-col justify-center bg-gray-500 gap-6 rounded-lg px-6 py-10 md:w-2/5 md:px-20">
+
           <p className={`${lusitana.className} text-xl text-white md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-900">
@@ -30,20 +30,21 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <div className='pt-8 pb-12'>
           {/* Add Hero Images Here */}
           <Image src="/hero-desktop.png"
           width={1000}
           height={760}
-          priority={true}
           className="hidden md:block"
           alt="Screenshots of the dashboard project showing desktop version"
-          />
+          /></div>
+          <div className='pt-6 pb-6"'>
           <Image src="/hero-mobile.png"
           width={560}
           height={620}
-          priority={true}
           className="hidden md:block"
           alt="Screenshots of the dashboard project showing mobile version"/>
+          </div>
         </div>
       </div>
     </main>
