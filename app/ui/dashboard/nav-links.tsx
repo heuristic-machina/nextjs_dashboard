944a13a9dc52eda8380322a5f7e6fc1d87d7152a
heuristic_machina <you@example.com>
2024-02-05 13:29:16 -0700

    dashboard components data retrieval

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,9 +31,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-300 p-3 text-sm font-medium hover:bg-green-300 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-300 p-3 text-sm font-medium hover:bg-green-400 hover:text-white md:flex-none md:justify-start md:p-2 md:px-3",
             {
-              'bg-green-300 text-slate-800': pathname === link.href,
+              'bg-green-400 text-slate-800': pathname === link.href,
             },
             )}
           >
