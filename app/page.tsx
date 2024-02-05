4817926a6db213670e0aa58bf5069b8b0ffb8e3d
heuristic_machina <you@example.com>
2024-02-05 02:24:46 -0700

    element placement

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,14 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 
-
 export default function Page() {
   return (
     <main className="flex dark:bg-slate-800 min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center bg-gray-500 gap-6 rounded-lg px-6 py-10 md:w-2/5 md:px-20">
-
+      <div className="mt-4 flex grow items-center flex-col gap-4 md:flex-row">
+        <div className="flex flex-col bg-gray-500 gap-6 rounded-lg px-6 py-10 md:w-2/5 md:px-20">
           <p className={`${lusitana.className} text-xl text-white md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-900">
@@ -29,8 +27,8 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <div className='pt-8 pb-12'>
+        <div className="flex items-center p-6 md:w-3/5 md:px-28 md:py-12">
+          <div className='py-12'>
           {/* Add Hero Images Here */}
           <Image src="/hero-desktop.png"
           width={1000}
@@ -38,7 +36,7 @@ export default function Page() {
           className="hidden md:block"
           alt="Screenshots of the dashboard project showing desktop version"
           /></div>
-          <div className='pt-6 pb-6"'>
+          <div className='py-8"'>
           <Image src="/hero-mobile.png"
           width={560}
           height={620}
