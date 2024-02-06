f7d2629874c647b6253df12d8f01aca147ae825e
heuristic_machina <you@example.com>
2024-02-06 13:53:46 -0700

    invoice form action and validation implemented

---
+++ app/lib/actions.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+// validate formData schema before saving to db 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+//pass data to createInvoice to validate the types
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    // convert to cents to eliminate JS floating-point errors in db
+    const amountInCents = amount * 100;
+    //sql date format
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+    // Test it out:
+    // console.log(rawFormData);
+}
