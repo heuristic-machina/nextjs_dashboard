3aa8981f4809f32eb94a0d2bd4e89f105bfa2612
heuristic_machina <machine_learner@yahoo.com>
2024-02-08 20:40:28 -0700

    skeleton fallback and file cleanup

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,9 @@
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
 import CustomersTable from '@/app/ui/customers/table';
 import { Metadata } from 'next';
 import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 export const metadata: Metadata = {
   title: 'Customers',
 };
@@ -17,12 +20,14 @@ export default async function CustomersPage({
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        </div>
-        <Suspense fallback={<p>Loading...</p>}>
-          <CustomersTable query={query} />
-        </Suspense>
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
       </div>
-    </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+      <Search placeholder="Search customers..." />
+            </div>
+            <Suspense fallback={<InvoicesTableSkeleton />}>
+              <CustomersTable query={query} />
+            </Suspense>
+          </div>
   );
 }
