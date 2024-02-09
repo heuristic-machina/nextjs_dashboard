55f96f5d477d3eca8068d3ee5d16c22e81a736a9
heuristic_machina <you@example.com>
2024-02-08 17:25:20 -0700

    customer search fallback implemented

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,30 @@
-import { lusitana } from '@/app/ui/fonts';
-
+import CustomersTable from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
 import { Metadata } from 'next';
- 
+import { Suspense } from 'react';
+import { lusitana } from '@/app/ui/fonts';
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+export default async function CustomersPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        </div>
+        <Suspense fallback={<p>Loading...</p>}>
+          <CustomersTable query={query} />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,6 +1,7 @@
 // This file contains type definitions for your data.
 // It describes the shape of the data, and what data type each property should accept.
-// These types are generated automatically if you're using an ORM such as Prisma.
+// For simplicity of teaching, we're manually defining these types.
+// However, these types are generated automatically if you're using an ORM such as Prisma.
 export type User = {
   id: string;
   name: string;
@@ -54,7 +55,7 @@ export type InvoicesTable = {
   status: 'pending' | 'paid';
 };
 
-export type CustomersTableType = {
+export type CustomersTable = {
   id: string;
   name: string;
   email: string;
